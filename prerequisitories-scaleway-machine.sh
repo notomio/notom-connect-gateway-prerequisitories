e0d6152326ac94be5c074412cf7e16145e52ed41
Charles Azam <azamcharles0@gmail.com>
2026-03-04 16:19:02 +0000

    update script

--- prerequisitories-scaleway-machine.sh
+++ prerequisitories-scaleway-machine.sh
@@ -24,6 +24,22 @@ fi
 # you can do that from VSCODE
 # then you can run the following commands:
 
+echo ""
+echo "============================================================"
+echo "  User '$USERNAME' has been created successfully."
+echo "  Please RECONNECT to this machine and LOG IN AS '$USERNAME'."
+echo "  You can do that from VSCode."
+echo "  Then re-run this script as '$USERNAME'."
+echo "============================================================"
+echo ""
+read -p "Press Enter to continue (only if you are already logged in as '$USERNAME')..."
+
+if [ "$(whoami)" != "$USERNAME" ]; then
+    echo "ERROR: You are currently logged in as '$(whoami)', not '$USERNAME'."
+    echo "Please reconnect as '$USERNAME' and re-run this script."
+    exit 1
+fi
+
 sudo chown $USER:$USER /opt
 git clone https://github.com/notomio/notom-connect-gateway.git /opt/notom-connect-gateway
 
